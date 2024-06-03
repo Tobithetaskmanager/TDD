eb70fa16e4f5d08800b42403ec699bed2ad9c301
Tobey <118977271+Tobithetaskmanager@users.noreply.github.com>
2024-06-03 17:17:44 +0200

    Trial Test for Elixir quality worked

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,9 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("should decrease quality by 1", function() {
+    items = [ new Item("Elixir of the Mongoose", 5, 7) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].quality).toEqual(6);
   });
 
 });
